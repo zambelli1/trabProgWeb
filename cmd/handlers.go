068c04f82b67166c642157df6d6535efb6477079
Vitalizera <88995481+Vitalizera@users.noreply.github.com>
2022-06-08 21:28:03 -0300

    Atualizando handlers

--- cmd/handlers.go
+++ cmd/handlers.go
@@ -44,3 +44,58 @@ func CreateProduct(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Falha na criação do produto"))
 	}
 }
+
+func ShowProduct(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("ShowProduct só aceita requesição do tipo GET"))
+		return
+	}
+
+	var produtos []Product
+	result, err := mysql.SelectProducts()
+
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	for result.Next() {
+		var produto Product
+		result.Scan(&produto.Id, &produto.Nome, &produto.Marca, &produto.Quantidade, &produto.Preco)
+		produtos = append(produtos, produto)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(produtos)
+}
+
+func UpdateProduct(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("UpdateProduct só aceita requesição do tipo POST"))
+		return
+	}
+
+	bodyDecoder := json.NewDecoder(r.Body)
+
+	var produto Product
+	bodyDecoder.Decode(&produto)
+
+	if produto.Id <= 0 || produto.Nome == "" || produto.Marca == "" || produto.Quantidade <= 0 || produto.Preco <= 0 {
+		w.Write([]byte("Precisa informar Id, Nome, Marca, Quantidade e Preco do produto para atualizar"))
+		return
+	}
+
+	result, err := mysql.UpdateProduct(produto.Id, produto.Nome, produto.Marca, produto.Quantidade, produto.Preco)
+
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	if result {
+		w.Write([]byte("Produto atualizado com sucesso"))
+	} else {
+		w.Write([]byte("Produto não encontrado ou nenhum campo foi alterado"))
+	}
+}
